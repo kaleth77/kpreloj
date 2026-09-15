042b50c31718005c2a40cc79467e86b5141ca75a
kaleth77 <kalecitopd@gmail.com>
2026-09-15 17:04:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function consultar(producto, precioTexto) {
+function consultar(producto, precioTexto, imgUrl) {
     let numero = "573126714681";
     let mensaje = "Hola, quiero consultar por: " + producto;
     if (precioTexto) {
         mensaje += " (" + precioTexto + ")";
+    }
+    if (imgUrl) {
+        mensaje += "\n" + imgUrl;
     }
     let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensaje);
     window.open(url, "_blank");
